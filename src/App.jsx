ed463c3af60bd10b51a55d62dbe8078fa45dad91
eklavya-skr <hesedskr2022@gmail.com>
2024-03-28 19:41:10 +0530

    create pages and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+ import Home from "./pages/Home.jsx";
+import Offers from './pages/Offers.jsx';
+import SignIn from './pages/SignIn.jsx';
+import SignUp from './pages/SignUp.jsx';
+ import Profile from './pages/Profile.jsx';
+import ForgotPassword from './pages/ForgotPassword.jsx';
+
 
 function App() {
-  const [count, setCount] = useState(0);
-
+ 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-red-300">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <nav>
+      <ul className='flex justify-between'>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/offers">Offers</Link></li>
+        <li><Link to="/signin">signin</Link></li>
+        <li><Link to="/signup">signup</Link></li>
+        <li><Link to="/profile">profile</Link></li>
+        <li><Link to="/forgotpasssword">forgotpasssword</Link></li> 
+       
+      </ul>
+    </nav>
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/offers" element={<Offers />} />
+    <Route path="/signin" element={<SignIn />} />
+    <Route path="/signup" element={<SignUp />} />
+    <Route path="/profile" element={<Profile />} />
+    <Route path="/forgot" element={<ForgotPassword />} /> 
+    </Routes>
     </>
+
   );
 }
 
